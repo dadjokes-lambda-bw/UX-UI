ca260f95b57412f84fdc04919282000022a76fcb
Kathryn Rhoads <kathrynrhoads@Kathryns-MacBook-Pro-2.local>
2019-06-26 15:58:44 -0400

    accordian

--- js/accordian.js
+++ js/accordian.js
@@ -6,8 +6,8 @@ class Accordian {
         // console.log(this)
         this.autoClose = accordian.dataset.autoClose === "true" ? true : false;
         // console.log(this.autoClose)
-        this.panelButtons = accordian.querySelectorAll('.panel-buttons')
-        console.log(this.panelButtons)
+        // this.panelButtons = accordian.querySelectorAll('.panel-buttons')
+        // console.log(this.panelButtons)
         this.panels = accordian.querySelectorAll('.panel');
         // console.log(this.panels)
         this.panels.forEach(panel => new Panel(panel, this))
@@ -15,7 +15,7 @@ class Accordian {
     }
     collapse(){
         if(this.autoClose){
-            let currentPanel = this.accordian.querySelector('.panel-content', '.toggle-on')
+            const currentPanel = this.accordian.querySelector('.panel-content','.toggle-on')
             if (currentPanel) {
                 currentPanel.classList.remove('toggle-on')
         }
@@ -26,17 +26,18 @@ class Accordian {
 class Panel {
     constructor(panel, parentAccordian) {
         this.panel = panel;
-        // console.log(this.panel)
+        // console.log(this)
         this.parentAccordian = parentAccordian;
-        this.panelButtons = panel.querySelector('panel-buttons')
+        this.panelButtons = this.panel.querySelector('.panel-buttons')
         // console.log(this.panelButtons)
-        this.openButton = panel.querySelector('.panel-btn-open')
-        console.log(this.openButton)
-        this.closeButton = panel.querySelector('.panel-btn-close')
-        console.log(this.closeButton)
-        this.panelContent = panel.querySelector('.panel-content')
-        this.openButton.addEventListener('click', () => this.togglePanel())
-        this.closeButton.addEventListener('click', () => this.togglePanel())
+        this.openButton = this.panel.querySelector('.panel-btn-open')
+        // console.log(this.openButton)
+         this.closeButton = this.panel.querySelector('.panel-btn-close')
+        // console.log(this.closeButton)
+        this.panelContent = this.panel.querySelector('.panel-content')
+        // this.openButton.addEventListener('click', () => this.togglePanel())
+        // this.closeButton.addEventListener('click', () => this.togglePanel())
+        this.panelButtons.addEventListener('click', () => this.togglePanel())
     
     }
     togglePanel(){
